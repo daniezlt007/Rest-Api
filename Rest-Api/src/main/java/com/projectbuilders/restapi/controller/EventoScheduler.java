cff4f4cf99296a02321d62661bac7214335e5402
Daniel da Silva <danielzt007@outlook.com>
2022-04-03 21:07:44 -0300

    Informado a Cron Expression

--- Rest-Api/src/main/java/com/projectbuilders/restapi/controller/EventoScheduler.java
+++ Rest-Api/src/main/java/com/projectbuilders/restapi/controller/EventoScheduler.java
@@ -1,12 +1,21 @@
 package com.projectbuilders.restapi.controller;
 
-import com.projectbuilders.restapi.model.EventoAppsFlyer;
 import com.projectbuilders.restapi.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+
+/*
+Cron Expression se resume em: <segundo> <minuto> <hora> <dia-do-mes> <mes> <ano>
+
+0/30 * * * * * = 30 segundos
+0 0/1 * * * *  = 1 minuto
+0 0/5 * * * *  = 5 minutos
+0 0/10 * * * * = 10 minutos
+0 0/15 * * * * = 15 minutos
+0 0 0/1 * * *  = 60 minutos
+*/
 
 @Component
 public class EventoScheduler {
